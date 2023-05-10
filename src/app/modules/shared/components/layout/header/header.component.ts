3547339a1ca6c4cbe8b2cebe9040be706656d8bd
mlichlundia <mlichlundiapl@gmail.com>
2023-05-10 17:14:32 +0200

    feat: implement back button functionality

--- src/app/modules/shared/components/layout/header/header.component.ts
+++ src/app/modules/shared/components/layout/header/header.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { Location } from '@angular/common'
+import { NavigationEnd, Router } from '@angular/router'
+import { filter } from 'rxjs'
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  public isBackShown: boolean = false
+
+  constructor(private location: Location, private router: Router) {
+    this.router.events
+      .pipe(
+        filter((event): event is NavigationEnd => event instanceof NavigationEnd)
+      ).subscribe((event: NavigationEnd) => {
+      if (event.url === '/') {
+        this.isBackShown = false
+        return
+      }
+
+      this.isBackShown = true
+    })
+  }
+
+  public goBack() {
+    this.location.back()
+  }
+}
